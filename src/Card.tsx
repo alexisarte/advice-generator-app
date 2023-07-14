654edfa8395bb999c930cb5e2413004443abfcc7
alexis <alexisrodriguezarteaga@gmail.com>
2023-07-14 18:11:59 -0300

    Addd styles and fetch

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,10 +1,35 @@
+import { useState } from 'react';
+
+type Advice = {
+  slip: {
+    id: number;
+    advice: string;
+  };
+};
+
 const Card = () => {
+  const [advice, setAdvice] = useState<Advice | null>(null);
+
+  const handleOnclick = async () => {
+    try {
+      const response = await fetch('https://api.adviceslip.com/advice');
+      const data = await response.json();
+      setAdvice(data);
+    } catch (error) {
+      console.log('error');
+    }
+  };
+
   return (
     <div className="flex items-center justify-center h-screen bg-dark-blue text-center">
-      <div className="bg-grayish-blue p-4 rounded-xl w-1/4">
-        <h1 className="text-neon-green">Advice #117</h1>
-        <p className="text[28px]">"It is easy to sit up an take notice, what's diffucult is getting up and talking action."</p>
-        <button className="bg-neon-green text-dark-blue rounded-full p-4">New</button>
+      <div className="bg-grayish-blue p-4 rounded-xl md:w-1/4 w-full m-4">
+        <h1 className="text-neon-green">ADVICE #{advice?.slip.id}</h1>
+        <p className="text[28px]">"{advice?.slip.advice}"</p>
+        <div className="bg-white h-[0.5px]"></div>
+        <button
+          className="bg-neon-green h-11 rounded-full w-11 hover:shadow-[0_0_60px_1px_rgba(0,255,0,0.547)] m-4"
+          onClick={handleOnclick}
+        ></button>
       </div>
     </div>
   );
